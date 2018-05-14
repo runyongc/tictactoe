8679b748495b43313c1af42e19f747db7411c18f
Grant Runyon <runyon_gc@newriver.edu>
2018-05-14 15:55:59 -0400

    HTML Changes

--- app.rb
+++ app.rb
@@ -34,6 +34,7 @@ require_relative 'tictactoe.rb'
   
   get '/auth/:provider/callback' do
     session[:profile_picture] = request.env['omniauth.auth']['info']['image']
+    session[:name] = request.env['omniauth.auth']['info']['name']
     redirect '/newgame'
   end
   
