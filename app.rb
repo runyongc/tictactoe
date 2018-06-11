b2588764f2b5563553bc1c0c127a9a5ff4f0d065
Grant Runyon <runyon_gc@newriver.edu>
2018-06-11 17:02:25 -0400

    Added jQuery and Ajax for page refresh.

--- app.rb
+++ app.rb
@@ -7,93 +7,95 @@ require 'omniauth-google-oauth2'
 require_relative 'tictactoe.rb'
 
 
-  configure do
-    use Rack::Session::Pool
-    set :inline_templates, true
-  end
-  
-  use OmniAuth::Builder do
-    provider :facebook, '1283085635154950','a5249f2819d8857a531a73ed31a7c29e'
-    provider :google_oauth2, '763470417567-ki9uikdee72fu5ab53ol611n2dm2pmmh.apps.googleusercontent.com', 'azKGBJvvJzEpgJ8XsngZkCTP'
-    #provider :att, 'client_id', 'client_secret'
+configure do
+  use Rack::Session::Pool
+  set :inline_templates, true
+end
+
+use OmniAuth::Builder do
+  provider :facebook, '1283085635154950', 'a5249f2819d8857a531a73ed31a7c29e'
+  provider :google_oauth2, '763470417567-ki9uikdee72fu5ab53ol611n2dm2pmmh.apps.googleusercontent.com', 'azKGBJvvJzEpgJ8XsngZkCTP'
+  #provider :att, 'client_id', 'client_secret'
+end
+
+helpers do
+  def database_query
+    db = PG::Connection.new(ENV['DATABASE_URL'])
+    session[:highscore] = db.exec('SELECT * FROM tictac_highscores;').to_a
   end
 
-  helpers do
-    def database_query
-      db = PG::Connection.new(ENV['DATABASE_URL'])
-      session[:highscore] = db.exec('SELECT * FROM tictac_highscores;').to_a
-    end
-
-    def update_highscores(column)
-      db = PG::Connection.new(ENV['DATABASE_URL'])
-      columnname = column
-      db.exec("UPDATE tictac_highscores SET #{columnname} = #{columnname} + 1")
-    end
-  end
-  get '/' do
-    erb :index
-  end
-  
-  get '/auth/:provider/callback' do
-    session[:authenticated] = true
-    session[:profile_picture] = request.env['omniauth.auth']['info']['image']
-    session[:name] = request.env['omniauth.auth']['info']['name']
-    redirect '/newgame'
-  end
-  
-  get '/newgame' do
-    erb :newgame
+  def update_highscores(column)
+    db = PG::Connection.new(ENV['DATABASE_URL'])
+    columnname = column
+    db.exec("UPDATE tictac_highscores SET #{columnname} = #{columnname} + 1")
   end
 
-  post '/startgame' do
-    @currentGame = TicTacToe.new(params[:difficulty], params[:gridsize])
-    session[:game] = @currentGame
-    redirect '/tictactoe'
-  end
+end
 
-  get '/tictactoe' do
-    if session[:game].gridsize == "3"
-      erb :tictactoe
-    elsif session[:game].gridsize == "4"
-      erb :tictactoe4x4
-    end
-  end
+get '/' do
+  erb :index
+end
 
-  get '/playermoved' do
-    session[:game].playGame(params[:tttmove])
-    if session[:game].gridsize == "3"
-      erb :tictactoe
-    elsif session[:game].gridsize == "4"
-      erb :tictactoe4x4
-    end
-  end
+get '/auth/:provider/callback' do
+  session[:authenticated] = true
+  session[:profile_picture] = request.env['omniauth.auth']['info']['image']
+  session[:name] = request.env['omniauth.auth']['info']['name']
+  redirect '/newgame'
+end
 
-  get '/auth/failure' do
-    erb "<h1>Authentication Failed:</h1><h3>message:<h3> <pre>#{params}</pre>"
-  end
-  
-  get '/auth/:provider/deauthorized' do
-    erb "#{params[:provider]} has deauthorized this app."
-  end
-  
-  get '/protected' do
-    throw(:halt, [401, "Not authorized\n"]) unless session[:authenticated]
-    erb "<a href='/logout'>Logout</a>"
-  end
-  
-  get '/logout' do
-    session[:authenticated] = false
-    redirect '/'
-  end
-  
-  get '/privacy' do
-    erb :privacy
-  end
+get '/newgame' do
+  erb :newgame
+end
 
-  get '/tos' do
-    erb :tos
-  end
+post '/startgame' do
+  @currentGame = TicTacToe.new(params[:difficulty], params[:gridsize])
+  session[:game] = @currentGame
+  redirect '/tictactoe'
+end
 
-  get '/testing' do
-    erb :privacy
+get '/tictactoe' do
+  if session[:game].gridsize == "3"
+    erb :tictactoe
+  elsif session[:game].gridsize == "4"
+    erb :tictactoe4x4
   end
+end
+
+get '/playermoved' do
+  session[:game].playGame(params[:tttmove])
+  p "#{session[:game].currentBoard}"
+  if session[:game].gridsize == "3"
+    erb :tictactoe
+  elsif session[:game].gridsize == "4"
+    erb :tictactoe4x4
+  else
+    erb :tictactoe
+
+  end
+end
+
+get '/auth/failure' do
+  erb "<h1>Authentication Failed:</h1><h3>message:<h3> <pre>#{params}</pre>"
+end
+
+get '/auth/:provider/deauthorized' do
+  erb "#{params[:provider]} has deauthorized this app."
+end
+
+get '/protected' do
+  throw(:halt, [401, "Not authorized\n"]) unless session[:authenticated]
+  erb "<a href='/logout'>Logout</a>"
+end
+
+get '/logout' do
+  session[:authenticated] = false
+  redirect '/'
+end
+
+get '/privacy' do
+  erb :privacy
+end
+
+get '/tos' do
+  erb :tos
+end
--- tictactoe.rb
+++ tictactoe.rb
@@ -1,7 +1,7 @@
 class PerfectComputer
 
   def choose_space(game)
-  	@best_score = {}
+    @best_score = {}
     negamax(game)
     p @best_score.inspect
     @best_score.max_by(&:last).first
@@ -11,20 +11,18 @@ class PerfectComputer
   private
 
   def negamax(game, depth = 0, alpha = -1000, beta = 1000, color = 1)
-    
-    if game.game_over?
-        	return color * score_scenarios(depth, game)
-    end
-    
+
+    return color * score_scenarios(depth, game) if game.game_over?
+
     max = -1000
 
     game.check_available_spaces.each do |space|
-    	if depth >= 9
-    		break
-    	end
+      if depth >= 9
+        break
+      end
       game.board[space] = game.current_player
       game.current_player = game.current_player == 'X' ? 'O' : 'X'
-      negamax_value = -negamax(game, depth+1, -beta, -alpha, -color)
+      negamax_value = -negamax(game, depth + 1, -beta, -alpha, -color)
       game.board[space] = nil
       game.current_player = game.current_player == 'X' ? 'O' : 'X'
       p "depth: #{depth}"
@@ -54,7 +52,7 @@ class GameState
     self.current_player = current_player
     self.board = board
   end
-  
+
   def place_marker(space, marker)
     board[space] = marker
   end
@@ -73,150 +71,153 @@ class GameState
   end
 
   def draw?
-   if board.length == 16
-       board.compact.size == 16 && winner.nil?
-   elsif board.length == 9
-   	   board.compact.size == 9 && winner.nil?
-   	end
+    if board.length == 16
+      board.compact.size == 16 && winner.nil?
+    elsif board.length == 9
+      board.compact.size == 9 && winner.nil?
+    end
   end
 
   def winner
     if board.length == 9
-      @winner = nil	
+      @winner = nil
       @winner ||= [
-         # horizontal wins
-         [0, 1, 2],
-         [3, 4, 5],
-         [6, 7, 8],
+          # horizontal wins
+          [0, 1, 2],
+          [3, 4, 5],
+          [6, 7, 8],
 
-         # vertical wins
-         [0, 3, 6],
-         [1, 4, 7],
-         [2, 5, 8],
+          # vertical wins
+          [0, 3, 6],
+          [1, 4, 7],
+          [2, 5, 8],
 
-         # diagonal wins
-         [0, 4, 8],
-         [6, 4, 2]
-     ].collect { |positions|
-      ( board[positions[0]] == board[positions[1]] &&
-        board[positions[1]] == board[positions[2]] &&
-        board[positions[0]]) || nil }.compact.first
+          # diagonal wins
+          [0, 4, 8],
+          [6, 4, 2]
+      ].collect {|positions|
+        (board[positions[0]] == board[positions[1]] &&
+            board[positions[1]] == board[positions[2]] &&
+            board[positions[0]]) || nil}.compact.first
     elsif board.length == 16
-      @winner = nil	
+      @winner = nil
       @winner ||= [
-         # horizontal wins
-         [0, 1, 2, 3],
-         [4, 5, 6, 7],
-         [8, 9, 10, 11],
-         [12, 13, 14, 15],
+          # horizontal wins
+          [0, 1, 2, 3],
+          [4, 5, 6, 7],
+          [8, 9, 10, 11],
+          [12, 13, 14, 15],
 
-         # vertical wins
-         [0, 4, 8, 12],
-         [1, 5, 9, 13],
-         [2, 6, 10, 14],
-         [3, 7, 11, 15],
+          # vertical wins
+          [0, 4, 8, 12],
+          [1, 5, 9, 13],
+          [2, 6, 10, 14],
+          [3, 7, 11, 15],
 
-         # diagonal wins
-         [0, 5, 10, 15],
-         [12, 9, 6, 3]
-     ].collect { |positions| 
-      ( board[positions[0]] == board[positions[1]] &&
-        board[positions[1]] == board[positions[2]] && 
-        board[positions[2]] == board[positions[3]] && 
-        board[positions[0]]) || nil }.compact.first
+          # diagonal wins
+          [0, 5, 10, 15],
+          [12, 9, 6, 3]
+      ].collect {|positions|
+        (board[positions[0]] == board[positions[1]] &&
+            board[positions[1]] == board[positions[2]] &&
+            board[positions[2]] == board[positions[3]] &&
+            board[positions[0]]) || nil}.compact.first
     end
   end
 end
 
 class TicTacToe
-	attr_accessor :currentBoard, :difficulty, :gridsize
-	def initialize(difficulty, gridsize)
-		@difficulty = difficulty
-		@gridsize = gridsize
-		setupGame
-		@currentBoard = @game_state.board
-	end
+  attr_accessor :currentBoard, :difficulty, :gridsize, :finalBoard
 
-	def setupGame
-		if @difficulty == "hard" && @gridsize == "4"
-			@game_state = GameState.new('X', Array.new(16))
-			@ai = PerfectComputer.new
-		elsif @difficulty == "easy" && @gridsize == "4"
-			@game_state = GameState.new('X', Array.new(16))
-		elsif @difficulty == "hard" && @gridsize == "3"
-			@ai = PerfectComputer.new
-			@game_state = GameState.new('X', Array.new(9))
-		elsif @difficulty == "easy" && @gridsize == "3"
-			@game_state = GameState.new('X', Array.new(9))
-		end
-	end
+  def initialize(difficulty, gridsize)
+    @difficulty = difficulty
+    @gridsize = gridsize
+    setupGame
+    @currentBoard = @game_state.board
+    @finalBoard = []
+  end
 
-	def ai_move
-			move = @ai.choose_space(@game_state)
-			@game_state.board[move] = 'X'
-			@game_state.winner
-	end
+  def setupGame
+    if @difficulty == "hard" && @gridsize == "4"
+      @game_state = GameState.new('X', Array.new(16))
+      @ai = PerfectComputer.new
+    elsif @difficulty == "easy" && @gridsize == "4"
+      @game_state = GameState.new('X', Array.new(16))
+    elsif @difficulty == "hard" && @gridsize == "3"
+      @ai = PerfectComputer.new
+      @game_state = GameState.new('X', Array.new(9))
+    elsif @difficulty == "easy" && @gridsize == "3"
+      @game_state = GameState.new('X', Array.new(9))
+    end
+  end
 
-	def player_move(string)
-		move = string.to_i
-		@currentBoard[move] = 'O'
-	end
+  def ai_move
+    move = @ai.choose_space(@game_state)
+    @game_state.board[move] = 'X'
+    @game_state.winner
+  end
 
-	def available_spaces
-		available = []
-		@currentBoard.each_index do |ndx|
-			available << ndx if @currentBoard[ndx].nil?
-		end
-		available
-	end
+  def player_move(string)
+    move = string.to_i
+    @currentBoard[move] = 'O'
+  end
 
-	def winner
-		if @game_state.game_over?
-		if @game_state.winner == "X"
-			return "X"
-		elsif @game_state.winner
-			return "O"
+  def available_spaces
+    available = []
+    @currentBoard.each_index do |ndx|
+      available << ndx if @currentBoard[ndx].nil?
+    end
+    available
+  end
 
-		elsif @game_state.draw?
-			return "draw"
-		else return nil
-		end
-	end
-	end
+  def winner
+    if @game_state.game_over?
+      if @game_state.winner == "X"
+        return "X"
+      elsif @game_state.winner
+        return "O"
 
-	def render_board
-		@currentBoard = @game_state.board
-	end
+      elsif @game_state.draw?
+        return "draw"
+      else
+        return nil
+      end
+    end
+  end
 
-	def ai_move_easy
-		if @difficulty == "easy" && !@game_state.game_over?
-			move = available_spaces.sample
-			@currentBoard[move] = "X"
-		end
-	end
+  def render_board
+    @finalBoard = @game_state.board
+  end
 
-	def hard_difficulty_round(string)
-		if @difficulty == "hard" && !@game_state.game_over?
-			player_move(string)
-			if available_spaces.length > 0
-			ai_move
-			render_board
-		end
-		end
-	end
+  def ai_move_easy
+    if @difficulty == "easy" && !@game_state.game_over?
+      move = available_spaces.sample
+      @currentBoard[move] = "X"
+    end
+  end
 
-	def easy_difficulty_round(string)
-		if @difficulty == "easy" && !@game_state.game_over?
-			player_move(string)
-			ai_move_easy
-			render_board
-		end
-	end
-	
-	def playGame(string)
-		hard_difficulty_round(string)
-		easy_difficulty_round(string)
-	end
+  def hard_difficulty_round(string)
+    if @difficulty == "hard" && !@game_state.game_over?
+      player_move(string)
+      if available_spaces.length > 0
+        ai_move
+        render_board
+      end
+    end
+  end
+
+  def easy_difficulty_round(string)
+    if @difficulty == "easy" && !@game_state.game_over?
+      player_move(string)
+      ai_move_easy
+      render_board
+    end
+  end
+
+  def playGame(string)
+    hard_difficulty_round(string)
+    easy_difficulty_round(string)
+  end
 end
 
 
